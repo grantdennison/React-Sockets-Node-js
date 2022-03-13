7a29c9b00d5b3f78e0843965eaea46b9c59897f9
Grant Dennison <grantdennison@yahoo.co.uk>
2022-03-13 02:51:14 +0000

    working time display multipal sockets

---
+++ socket-io-server/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+
+const app = express();
+// const cors = require("cors"); // not required by look of it
+app.use(index);
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+let interval;
+
+io.on("connection", (socket) => {
+  socket.join("Grants room"); //specify room
+  console.log(`New client connected ${interval}`);
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  console.log(socket.rooms);
+  console.log(socket.id); //socket id
+  socket.on("disconnect", () => {
+    console.log(`Client disconnected ${interval}`);
+    // clearInterval(interval);
+  });
+});
+
+const getApiAndEmit = (socket) => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  io.emit("FromAPI", response);
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
